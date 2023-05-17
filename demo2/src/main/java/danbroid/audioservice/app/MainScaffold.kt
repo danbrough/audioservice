163944b473417fdd184affed40aaa4cabe91d9fc
Dan Brough <dan@danbrough.org>
2023-05-18 10:01:21 +1200

    update

--- demo2/src/main/java/danbroid/audioservice/app/MainScaffold.kt
+++ demo2/src/main/java/danbroid/audioservice/app/MainScaffold.kt
@@ -3,21 +3,17 @@ package danbroid.audioservice.app
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.BottomSheetScaffold
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.PlayArrow
 import androidx.compose.material.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import danbroid.audio.library.AudioClientViewModel
+import danbroid.audioservice.app.ui.controls.BottomControls
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
@@ -36,21 +32,12 @@ fun MainScaffold(
     sheetBackgroundColor = MaterialTheme.colors.primary,
     sheetPeekHeight = sheetHeight + navBottom,
     sheetContent = {
-      Column(Modifier.fillMaxSize()) {
-        Row(
-          modifier = Modifier
-            .fillMaxWidth()
-            .padding(horizontal = 16.dp),
-          verticalAlignment = Alignment.CenterVertically
-        ) {
-          Text("Current song title", modifier = Modifier.weight(1f))
-          IconButton(onClick = { /*TODO play song*/ }) {
-            Icon(
-              Icons.Rounded.PlayArrow,
-              contentDescription = "Play"
-            )
-          }
-        }
+      Box(
+        Modifier
+          .fillMaxWidth()
+          .fillMaxHeight()
+      ) {
+        BottomControls(expanded = scaffoldState.bottomSheetState.isExpanded, audioClientModel)
       }
     }) {
     Box(
