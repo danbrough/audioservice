4033f8bde4bf4a32b4f5160d69a7fb2a6f631406
Dan Brough <dan@danbrough.org>
2023-05-18 10:41:58 +1200

    update

--- demo2/src/main/java/danbroid/audioservice/app/MainScaffold.kt
+++ demo2/src/main/java/danbroid/audioservice/app/MainScaffold.kt
@@ -3,23 +3,31 @@ package danbroid.audioservice.app
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.BottomSheetScaffold
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.PlayArrow
+import androidx.compose.material.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 import danbroid.audio.library.AudioClientViewModel
+import danbroid.audioservice.app.ui.controls.BottomControls
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun MainScaffold(
   audioClientModel: AudioClientViewModel
 ) {
+  val navBottom = with(LocalDensity.current) { WindowInsets.navigationBars.getBottom(this).toDp() }
+
+  val scaffoldState = rememberBottomSheetScaffoldState()
+  val sheetHeight = 50.dp
+
   BottomSheetScaffold(
+    sheetBackgroundColor = MaterialTheme.colors.primary,
+    sheetPeekHeight = sheetHeight + navBottom,
+
     sheetContent = {
       Column(Modifier.fillMaxSize()) {
         Row(
@@ -28,20 +36,15 @@ fun MainScaffold(
             .padding(horizontal = 16.dp),
           verticalAlignment = Alignment.CenterVertically
         ) {
-          Text("Current song title", modifier = Modifier.weight(1f))
-          IconButton(onClick = { /*TODO play song*/ }) {
-            Icon(
-              Icons.Rounded.PlayArrow,
-              contentDescription = "Play"
-            )
-          }
+          BottomControls(expanded = scaffoldState.bottomSheetState.isExpanded, audioClientModel)
         }
       }
     }) {
     Box(
       Modifier
         .fillMaxSize()
-        .padding(16.dp)) {
+        .padding(16.dp)
+    ) {
       Text("Main Screen Content")
     }
   }
